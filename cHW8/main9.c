42be227de8b60d390beb5375a0a29b5a224c40d6
AlisaShi <59513354+AlisaShi@users.noreply.github.com>
2023-01-14 17:39:31 +0800

    Create main9.c

---
+++ cHW8/main9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+
+  int i, j;
+  for (i - 0; i < 100, j != 3; i++, j++)
+    {
+      printf ("i=%d\n", i);
+    }
+
+  return 0;
+}
+//會停，於for loop中增加一行printf("%d",j);輸出j即可知道j雖未給初始值將從0開始
