9af92992f89b37a062e5a0e04c38c1a37209b633
AlisaShi <59513354+AlisaShi@users.noreply.github.com>
2023-01-14 17:42:21 +0800

    Update main7.c

--- cHW8/main7.c
+++ cHW8/main7.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/**#include <stdio.h>
 
 int main()
 {
@@ -33,6 +33,9 @@ int main()
         b=b/10;
         bsum=bsum+b%10;
         b=b/10;
+        }
+    return 0;
+}
         bsum=-bsum;
     }
     else{
@@ -47,4 +50,32 @@ int main()
     printf("a=%d\n",asum);
     printf("b=%d\n",bsum);
     return 0;
+}**/
+
+
+#include <stdio.h>
+int get_digit(int num) {
+return num%10;
+}
+int maino {
+int num = - 9990;
+int sum = 0;
+int negative = 0;
+if (num < 0) {
+negative = 1;
+num = 0 - num;
+}
+do {
+if (num < 10) {
+if (negative) {
+sum -= get_digit(num);
+}
+}
+else {
+sum += get_digit(num);
+}
+num /= 10;
+} while (num>0);
+printf("sum-%d\n", sum);
+return 0;
 }
